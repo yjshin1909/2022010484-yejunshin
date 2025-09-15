d98dee60829bcd94ed6fad05b88750dc6a956dc4
SimYeonJae <112994031+SimYeonJae@users.noreply.github.com>
2025-09-15 16:26:24 +0900

    09-15

---
+++ 09-15/09-15/main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("=== Basic Contants ===\n\n");
+	printf("Name\t: %s\n\n", "Sungwook");
+	printf("Initial\t: %c\n\n", 'S');
+	printf("Age\t: %d\n\n", 28);
+	printf("Score\t: %f\n\n", 93.5);
+	printf("Count\t: %u\n\n", 3000000000u);
+
+	return 0;
+}
